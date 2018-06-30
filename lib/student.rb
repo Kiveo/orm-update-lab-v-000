1adf360d740a0d54a12c23596cc830c31281f12f
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 05:03:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     student_row = DB[:conn].execute(sql, self.name)
     self.new_from_db(student_row)
