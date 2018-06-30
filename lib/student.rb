c92bd0ddba3b2e6d9ff085a583f830008f6b6827
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 04:19:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,25 +1,33 @@
 require_relative "../config/environment.rb"
 # Remember, you can access your database connection anywhere in this class with DB[:conn]
 class Student
-  attr_accessor :name, :grade 
-  attr_reader :id 
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id)
-    @name = name 
-    @grade = grade 
-    @id = id 
-  end 
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
-  def self.create_table 
-    #sql with here doc, to create the table in db. 
-    sql = <<-SQL 
+  def self.create_table
+    #sql with here doc, to create the table in db.
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade INTEGER  
+      grade INTEGER
       )
-      SQL 
+      SQL
     DB[:conn].execute(sql)
-  end 
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
+  end
+
 
 end
