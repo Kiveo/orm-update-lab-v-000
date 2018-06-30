c278322189b90f79e6a7ab9fcdba8ed22387fa1a
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 05:11:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,12 +61,12 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   #
-  # def self.find_by_name(name)
-  #   sql = <<-SQL
-  #     SELECT * FROM students WHERE name = ? LIMIT 1
-  #   SQL
-  #   DB[:conn].execute(sql, name).collect {|row| self.new_from_db(row)}.first
-  # end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).collect {|row| self.new_from_db(row)}.first
+  end
 
 
 end
