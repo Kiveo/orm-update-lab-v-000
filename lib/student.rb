64694b5bcbc5b600ae00e80d8ef07c46ba2c6391
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 05:10:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,10 +56,10 @@ class Student
   #   student
   # end
   #
-  # def update
-  #   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-  #   DB[:conn].execute(sql, self.name, self.grade, self.id)
-  # end
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   #
   # def self.find_by_name(name)
   #   sql = <<-SQL
