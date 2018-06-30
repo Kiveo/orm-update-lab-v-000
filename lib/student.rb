f830288d4c4903cff10ef211e9b369d46fea4ba7
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 05:12:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,20 +47,20 @@ class Student
     student.save
     student
   end
-  #
-  # def self.new_from_db(row)
-  #   student = self.new    #id, name, grade are row[0-2]
-  #   student.id = row[0]
-  #   student.name = row[1]
-  #   student.grade[2]
-  #   student
-  # end
-  #
+
+  def self.new_from_db(row)
+    student = self.new    #id, name, grade are row[0-2]
+    student.id = row[0]
+    student.name = row[1]
+    student.grade[2]
+    student
+  end
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-  #
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
